ae927df4daa1b11eda262758b5fcaeb5918e4a89
Victor Gou <victorgou.design@gmail.com>
2024-09-21 15:19:23 -0300

    linear propagation now wraps around the matrix

--- linear_propagation.bash
+++ linear_propagation.bash
@@ -32,37 +32,49 @@ BEGIN {
 }
 END {
     if (direction == "up") {
-        for (y = y_dim - 1; y >= 0; y--) {
+        for (x = 0; x < x_dim; x++) {
+            if (matrix[0][x] == 1) new_matrix[y_dim-1][x] = 1;
+            else new_matrix[y_dim-1][x] = 0;
+        }
+        for (y = y_dim - 2; y >= 0; y--) {
             for (x = 0; x < x_dim; x++) {
-                if (y == y_dim - 1) new_matrix[y][x] = 0;
-                else if (matrix[y+1][x] == 1) new_matrix[y][x] = 1;
+                if (matrix[y+1][x] == 1) new_matrix[y][x] = 1;
                 else if (matrix[y][x] == 1) new_matrix[y][x] = 0;
                 else new_matrix[y][x] = 0;
             }
         }
     } else if (direction == "down") {
-        for (y = 0; y < y_dim; y++) {
+        for (x = 0; x < x_dim; x++) {
+            if (matrix[y_dim-1][x] == 1) new_matrix[0][x] = 1;
+            else new_matrix[0][x] = 0;
+        }
+        for (y = 1; y < y_dim; y++) {
             for (x = 0; x < x_dim; x++) {
-                if (y == 0) new_matrix[y][x] = 0;
-                else if (matrix[y-1][x] == 1) new_matrix[y][x] = 1;
+                if (matrix[y-1][x] == 1) new_matrix[y][x] = 1;
                 else if (matrix[y][x] == 1) new_matrix[y][x] = 0;
                 else new_matrix[y][x] = 0;
             }
         }
     } else if (direction == "left") {
-        for (x = x_dim - 1; x >= 0; x--) {
+        for (y = 0; y < y_dim; y++) {
+            if (matrix[y][0] == 1) new_matrix[y][x_dim-1] = 1;
+            else new_matrix[y][x_dim-1] = 0;
+        }
+        for (x = x_dim - 2; x >= 0; x--) {
             for (y = 0; y < y_dim; y++) {
-                if (x == x_dim - 1) new_matrix[y][x] = 0;
-                else if (matrix[y][x+1] == 1) new_matrix[y][x] = 1;
+                if (matrix[y][x+1] == 1) new_matrix[y][x] = 1;
                 else if (matrix[y][x] == 1) new_matrix[y][x] = 0;
                 else new_matrix[y][x] = 0;
             }
         }
     } else if (direction == "right") {
-        for (x = 0; x < x_dim; x++) {
+        for (y = 0; y < y_dim; y++) {
+            if (matrix[y][x_dim-1] == 1) new_matrix[y][0] = 1;
+            else new_matrix[y][0] = 0;
+        }
+        for (x = 1; x < x_dim; x++) {
             for (y = 0; y < y_dim; y++) {
-                if (x == 0) new_matrix[y][x] = 0;
-                else if (matrix[y][x-1] == 1) new_matrix[y][x] = 1;
+                if (matrix[y][x-1] == 1) new_matrix[y][x] = 1;
                 else if (matrix[y][x] == 1) new_matrix[y][x] = 0;
                 else new_matrix[y][x] = 0;
             }
